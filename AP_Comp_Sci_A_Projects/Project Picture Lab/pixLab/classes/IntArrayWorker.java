daa7d32fc4e61f17227c171339e2df88e544e301
798566 <42599884+798566@users.noreply.github.com>
2018-11-19 10:33:32 -0800

    111918 end

--- AP_Comp_Sci_A_Projects/Project Picture Lab/pixLab/classes/IntArrayWorker.java
+++ AP_Comp_Sci_A_Projects/Project Picture Lab/pixLab/classes/IntArrayWorker.java
@@ -98,5 +98,39 @@ public class IntArrayWorker
       }
     }
   }
- 
+  
+  public int getCount(int n)
+  {
+      int count = 0;
+      for (int row = 0; row < matrix.length; row++) {
+          for(int col = 0; col < matrix[0].length; col++) {
+              if (matrix[row][col] == n){
+                  count = count + 1;
+              }
+          }
+      }
+      return count;
+  }
+  
+  public int getLargest(){
+      int largest = 0;
+      for (int row = 0; row < matrix.length; row++) {
+          for(int col = 0; col < matrix[0].length; col++) {
+              if (matrix[row][col] > largest){
+                  largest = matrix[row][col];
+              }
+          }
+      }
+      return largest;
+  }
+  
+  public int getColTotal(int c){
+      int total = 0;
+      for (int row = 0; row < matrix.length; row++)
+      {
+          total = total + matrix[row][c];
+          
+      }
+      return total;
+  }
 }
